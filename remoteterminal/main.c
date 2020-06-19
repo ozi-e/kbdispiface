d406ffba87ae76c975ad9314a702b12cd23c045d
Ozi <ozi.erdem@hotmail.com>
2020-06-19 11:21:39 +0200

    Exam commit

--- remoteterminal/main.c
+++ remoteterminal/main.c
@@ -1,4 +1,3 @@
-//  ZMQ Quest code (expired 20/04)
 #include "zhelpers.h"
 #include <unistd.h>
 #include <string.h>
@@ -21,45 +20,89 @@ int main (void)
 
     char cmdpath[] = "C:/WINDOWS/system32/cmd.exe";
     char temppath[1035];
-    char path[] = "E:/eJ3/NP/kbdispiface/build-remoteterminal-Desktop_Qt_5_14_1_MinGW_64_bit-Debug/fop.txt";
+    //char path[] = "E:/eJ3/NP/kbdispiface/build-remoteterminal-Desktop_Qt_5_14_1_MinGW_64_bit-Debug/fop.txt";
 
     FILE *fp;
 
     char buffer[256] = "";
 
     printf("Sending command\r\n");
-    strncpy(buffer, pushanswer_h, 128);
-    strncat(buffer, "Windows Remote Console has been started, please enter your command now.\r\n", 255);
-    s_send(pusher, buffer);
+    //strncpy(buffer, pushanswer_h, 128);
+    //strncat(buffer, "Windows Remote Console has been started, please enter your command now.\r\n", 255);
+    //s_send(pusher, buffer);
 
     while(1)
     {
-        char buffer[255] = "";
+        char *buffer;
         char temppath[255];
         printf("Receiving command\r\n");
-        zmq_recv(sub, buffer, 255, 0);
+        //zmq_recv(sub, buffer, 255, 0);
+        buffer = s_recv(sub);
+        char *rcvstr = strrchr(buffer, '>');
         printf("first\r\n");
+        //char *totalbuf = NULL; //Causes program crashing lmfao
+        //char totalbuf[2000];
 
-        strncpy(temppath, cmdpath, 255);
-        strncat(temppath, " ", 2);
-        char *cch = strrchr(buffer, '>');
-        strncpy(buffer, cch++, 1023);
-        strncat(temppath, buffer, 255);
-        printf("%s\r\n", temppath);
-        system(temppath);
-        fp = popen(temppath, "r");
-          if (fp == NULL) {
-            printf("Failed to run command\n" );
-            //exit(1);
-          }
-        /* Read the output a line at a time - output it. */
-        while (fgets(path, sizeof(path), fp) != NULL) {
-            printf("%s", path);
-            strncpy(buffer, pushanswer_h, 128);
-            strncat(buffer, path, 1035);
-            s_send(pusher, buffer);
+        //if (strstr(buffer, ".exe") != NULL)
+        if (buffer > 0)
+        {
+            char *pathtoexe;
+            //Removed delimiters and removed first char
+            //printf("Executed1\r\n");
+            //memcpy(pathtoexe, rcvstr, strlen(rcvstr));
+            memmove(rcvstr, rcvstr + 1, strlen(rcvstr));
+            //printf("Executed2\r\n");
+            sprintf(rcvstr, "%s > output.txt 2>&1", rcvstr);
+            system(rcvstr); //Runs command
+            printf("Executed\r\n");
+            fp = fopen("output.txt", "r");
+              if (fp == NULL) {
+                printf("Failed to run command\n" );
+                //exit(1);
+              }
+              char *tempb;
+            // Read the output a line at a time - output it.
+            //s_send(pusher, fgets(path, sizeof(path), fp));
+            char path[1035];
+            char altbuf[1050];
+            while ( fgets(path, sizeof(path), fp) != NULL) {
+                //sprintf(tempb, "%s", );
+                printf("%s", path);
+                //strncpy(buffer, pushanswer_h, 128);
+                //strncat(totalbuf, path, strlen(path));
+                sprintf(altbuf, "INTERFACE!>Ozi>%s", path);
+                s_send(pusher, altbuf); //Does absolutely not want to send
+                //printf("TEST");
+                //printf("doing stuff");
+            }
+            fclose(fp);
+            //printf("%s", totalbuf);
+            //s_send(pusher, totalbuf);
+            s_send(pusher, "INTERFACE!>Ozi>Done");
         }
-        pclose(fp);
+        else
+        {
+            /*strncpy(temppath, cmdpath, 255);
+            strncat(temppath, " ", 2);
+            char *cch = strrchr(buffer, '>');
+            strncpy(buffer, cch++, 1023);
+            strncat(temppath, buffer, 255);
+            printf("%s\r\n", temppath);
+            system(temppath);
+            fp = popen(temppath, "r");
+              if (fp == NULL) {
+                printf("Failed to run command\n" );
+                //exit(1);
+              }
+            // Read the output a line at a time - output it.
+            while (fgets(path, sizeof(path), fp) != NULL) {
+                printf("%s", path);
+                strncpy(buffer, pushanswer_h, 128);
+                strncat(buffer, path, 1035);
+                s_send(pusher, buffer);
+            }
+            pclose(fp);*/
+        }
     }
 
     /*printf("Sending [%u]...\r\n", 1);
--- remoteterminalclient/main.c
+++ remoteterminalclient/main.c
@@ -21,20 +21,45 @@ int main()
     char pubTopic[] = "INTERFACE?>Ozi>";
     char buffer[256];
     char scanVar[256];
+    char *bf;
 
     zmq_setsockopt(sub, ZMQ_SUBSCRIBE, subTopic, strlen(buffer));
+
+    int state = 1;
 
     while(1)
     {
         char buffer[256] = "";
-        zmq_recv(sub, buffer, 255, 0);
-        printf("%s\r\n", buffer);
+        //zmq_recv(sub, buffer, 255, 0);
+        //printf("%s\r\n", buffer);
 
         //zmq_send(pusher,)
-        strncpy(buffer, pubTopic, 22);
-        scanf("%s", scanVar);
-        strncat(buffer, scanVar , 250);
-        s_send(pusher, buffer);
+        //strncpy(buffer, pubTopic, 22);
+        //scanf("%s", scanVar);
+        //strncat(buffer, scanVar , 250);
+        //s_send(pusher, buffer);
+
+        if (state == 0)
+        {
+            bf = s_recv(sub);
+            if (strncmp(bf, "INTERFACE!>Ozi>Done", strlen(subTopic) + 4) == 0)
+            {
+                state = 1;
+            }
+            if (strncmp(bf, "INTERFACE!>Ozi>", strlen(subTopic)) == 0)
+            {
+                printf("%s\r\n", bf);
+            }
+        }
+        else if (state == 1)
+        {
+            strncpy(buffer, pubTopic, 22);
+            scanf("%[^\n]%*c", scanVar); //apparently reads newline
+            strncat(buffer, scanVar , 250);
+            s_send(pusher, buffer);
+            state = 0;
+        }
+
     }
     return 0;
 }
